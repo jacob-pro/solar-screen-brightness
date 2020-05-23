0348806eb884aec7d48375e9d8b269b57ab25d2b
Jacob Halsey <jacob@jhalsey.com>
2020-05-23 20:21:57 +0100

    show/hide console

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![windows_subsystem = "windows"]
 
+#[macro_use]
+extern crate lazy_static;
+
 mod assets;
 mod shell_icon;
 mod str_ext;
@@ -7,60 +10,71 @@ mod str_ext;
 // use cursive::views::{Dialog, TextView};
 // use cursive::Cursive;
 use winapi::um::consoleapi::{AllocConsole, SetConsoleCtrlHandler};
-use winapi::shared::minwindef::{DWORD, BOOL, TRUE};
-use winapi::um::wincon::{FreeConsole, CTRL_CLOSE_EVENT};
-use winapi::um::processthreadsapi::ExitThread;
-use crate::shell_icon::create_shell_icon;
-use winapi::um::winuser::{DispatchMessageA, TranslateMessage, GetMessageW};
+use winapi::shared::minwindef::{DWORD, BOOL, TRUE, FALSE};
+use winapi::shared::windef::HWND;
+use winapi::um::wincon::{FreeConsole, GetConsoleWindow};
+use crate::shell_icon::ShellIcon;
+use std::sync::Mutex;
 use winapi::shared::ntdef::NULL;
-use winapi::shared::windef::{HWND};
+use winapi::um::winuser::{GetSystemMenu, EnableMenuItem, SC_CLOSE, MF_ENABLED, MF_GRAYED, ShowWindow, SW_RESTORE, BringWindowToTop, SetForegroundWindow};
+
 
 fn main() {
 
 
-    create_shell_icon();
-    // // Creates the cursive root - required for every application.
-    // let mut siv = Cursive::crossterm().unwrap();
-    //
-    // // Creates a dialog with a single "Quit" button
-    // siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
-    //     .title("Cursive")
-    //     .button("Quit", |s| s.quit()));
-    //
-    //
-    // // Starts the event loop.
-    // siv.run();
+    let icon = ShellIcon::create();
+    icon.drive();
 
-
-    // unsafe {
-    //     if AllocConsole() != TRUE {panic!("Error opening console")};
-    //     if SetConsoleCtrlHandler(Some(ctrl_handler), TRUE) != TRUE {panic!("Error setting handler")};
-    // }
-
-    unsafe {
-        let mut msg = std::mem::MaybeUninit::uninit().assume_init();
-        loop {
-            let ret = GetMessageW(&mut msg, NULL as HWND, 0, 0);
-            match ret {
-                -1 => { panic!("GetMessage failed"); }
-                0 => { break }
-                _ => {
-                    TranslateMessage(&mut msg);
-                    DispatchMessageA(&mut msg);
-                }
-            }
-        }
-    }
 }
 
 // When the console window is being closed
-unsafe extern "system" fn ctrl_handler(dw_ctrl_type: DWORD) -> BOOL {
+unsafe extern "system" fn ctrl_handler(_dw_ctrl_type: DWORD) -> BOOL {
+    let mut visible = UI_VISIBLE.lock().unwrap();
     FreeConsole();
-    // Prevent ExitProcess by not returning the handler
-    if dw_ctrl_type == CTRL_CLOSE_EVENT {
-        ExitThread(0);
-    }
+    *visible = false;
     TRUE
 }
 
+lazy_static! {
+    static ref UI_VISIBLE: Mutex<bool> = Mutex::new(false);
+}
 
+pub fn display_ui() {
+
+    let mut visible = UI_VISIBLE.lock().unwrap();
+    if *visible {
+        unsafe {
+            let console_window = GetConsoleWindow();
+            if console_window == NULL as HWND { panic!("Null console window") };
+            ShowWindow(console_window, SW_RESTORE);
+            BringWindowToTop(console_window);
+            SetForegroundWindow(console_window);
+        }
+        return;
+    }
+
+    unsafe {
+        if AllocConsole() != TRUE {panic!("Error opening console")};
+        if SetConsoleCtrlHandler(Some(ctrl_handler), TRUE) != TRUE {panic!("Error setting handler")};
+        let console_window = GetConsoleWindow();
+        if console_window == NULL as HWND { panic!("Null console window") };
+        let console_menu = GetSystemMenu(console_window, FALSE);
+        EnableMenuItem(console_menu, SC_CLOSE as u32, MF_ENABLED | MF_GRAYED);
+    }
+
+    // std::thread::spawn(|| {
+    //     // Creates the cursive root - required for every application.
+    //     let mut siv = Cursive::crossterm().unwrap();
+    //
+    //     // Creates a dialog with a single "Quit" button
+    //     siv.add_layer(Dialog::around(TextView::new("Hello Dialog!"))
+    //         .title("Cursive")
+    //         .button("Quit", |s| s.quit()));
+    //
+    //
+    //     // Starts the event loop.
+    //     siv.run();
+    // });
+
+    *visible = true;
+}
--- src/shell_icon.rs
+++ src/shell_icon.rs
@@ -1,66 +1,102 @@
-use winapi::um::winuser::{DefWindowProcW, CreateWindowExW, WNDCLASSW, RegisterClassW, CW_USEDEFAULT, WS_OVERLAPPEDWINDOW, CreateIconFromResource, WM_APP, WM_LBUTTONUP, WM_RBUTTONUP};
+use winapi::um::winuser::{DefWindowProcW, CreateWindowExW, WNDCLASSW, RegisterClassW, CW_USEDEFAULT, WS_OVERLAPPEDWINDOW, CreateIconFromResource, WM_APP, WM_LBUTTONUP, WM_RBUTTONUP, GetMessageW, TranslateMessage, DispatchMessageW};
 use winapi::shared::minwindef::{UINT, WPARAM, LPARAM, LRESULT, TRUE, HINSTANCE, LOWORD, DWORD};
 use winapi::shared::windef::{HWND, HMENU, HICON};
-use winapi::um::shellapi::{NIM_ADD, NOTIFYICONDATAW, NOTIFYICONDATAW_u, Shell_NotifyIconW, NIF_MESSAGE, NIF_ICON, NIF_TIP, NOTIFYICON_VERSION_4, NIM_SETVERSION, NIN_SELECT};
+use winapi::um::shellapi::{NIM_ADD, NOTIFYICONDATAW, Shell_NotifyIconW, NIF_MESSAGE, NIF_ICON, NIF_TIP, NIM_DELETE};
 use winapi::shared::ntdef::{NULL};
 use winapi::um::libloaderapi::GetModuleHandleW;
 
 use crate::assets::Assets;
 use crate::str_ext::StrExt;
 use winapi::um::winnt::LPCWSTR;
+use crate::display_ui;
 
 const CALLBACK_MSG: UINT = WM_APP + 1;
 
-pub fn create_shell_icon() {
-    unsafe {
-        let hinstance = GetModuleHandleW( NULL as LPCWSTR );
-        if hinstance == NULL as HINSTANCE { panic!("Get hinstance failed") };
+pub struct ShellIcon (
+    NOTIFYICONDATAW
+);
 
-        let mut window_class: WNDCLASSW =  std::mem::MaybeUninit::zeroed().assume_init();
-        window_class.lpfnWndProc = Some(window_procedure);
-        window_class.hInstance = hinstance;
-        window_class.lpszClassName = "TrayHolder".to_wide().as_ptr();
-        let atom = RegisterClassW(&window_class);
-        if atom == 0 { panic!("Register window class failed") };
+impl ShellIcon {
 
-        let hwnd = CreateWindowExW(
-            0,
-            atom as *const u16,
-            "tray".to_wide().as_ptr(),
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            CW_USEDEFAULT,
-            NULL as HWND,
-            NULL as HMENU,
-            hinstance,
-            NULL);
-        if hwnd == NULL as HWND { panic!("Create window failed") };
+    pub fn create() -> Self {
+        unsafe {
+            let hinstance = GetModuleHandleW( NULL as LPCWSTR );
+            if hinstance == NULL as HINSTANCE { panic!("Get hinstance failed") };
 
-        let mut asset = Assets::get("icon-256.png").expect("Icon missing").into_owned();
-        let hicon = CreateIconFromResource(asset.as_mut_ptr(), asset.len() as u32, TRUE, 0x00030000);
-        if hicon == NULL as HICON { panic!("Failed to create icon") };
+            let mut window_class: WNDCLASSW =  std::mem::MaybeUninit::zeroed().assume_init();
+            window_class.lpfnWndProc = Some(tray_window_proc);
+            window_class.hInstance = hinstance;
+            window_class.lpszClassName = "TrayHolder".to_wide().as_ptr();
+            let atom = RegisterClassW(&window_class);
+            if atom == 0 { panic!("Register window class failed") };
 
-        let mut data: NOTIFYICONDATAW =  std::mem::MaybeUninit::zeroed().assume_init();
-        let mut name = "Solar Screen Brightness".to_wide();
-        name.resize(data.szTip.len(), 0);
-        let bytes = &name[..data.szTip.len()];
-        data.hWnd = hwnd;
-        data.hIcon = hicon;
-        data.uCallbackMessage = CALLBACK_MSG;
-        data.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
-        data.szTip.copy_from_slice(bytes);
-        if Shell_NotifyIconW(NIM_ADD, &mut data) != TRUE { panic!("Error creating tray icon") };
+            let hwnd = CreateWindowExW(
+                0,
+                atom as *const u16,
+                "tray".to_wide().as_ptr(),
+                WS_OVERLAPPEDWINDOW,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                NULL as HWND,
+                NULL as HMENU,
+                hinstance,
+                NULL);
+            if hwnd == NULL as HWND { panic!("Create window failed") };
+
+            let mut asset = Assets::get("icon-256.png").expect("Icon missing").into_owned();
+            let hicon = CreateIconFromResource(asset.as_mut_ptr(), asset.len() as u32, TRUE, 0x00030000);
+            if hicon == NULL as HICON { panic!("Failed to create icon") };
+
+            let mut data: NOTIFYICONDATAW =  std::mem::MaybeUninit::zeroed().assume_init();
+            let mut name = "Solar Screen Brightness".to_wide();
+            name.resize(data.szTip.len(), 0);
+            let bytes = &name[..data.szTip.len()];
+            data.hWnd = hwnd;
+            data.hIcon = hicon;
+            data.uCallbackMessage = CALLBACK_MSG;
+            data.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+            data.szTip.copy_from_slice(bytes);
+            if Shell_NotifyIconW(NIM_ADD, &mut data) != TRUE { panic!("Error creating tray icon") };
+            ShellIcon(data)
+        }
+    }
+
+    pub fn drive(&self) {
+        unsafe {
+            let mut msg = std::mem::MaybeUninit::uninit().assume_init();
+            loop {
+                let ret = GetMessageW(&mut msg, self.0.hWnd, 0, 0);
+                match ret {
+                    -1 => { panic!("GetMessage failed"); }
+                    0 => { break }
+                    _ => {
+                        TranslateMessage(&mut msg);
+                        DispatchMessageW(&mut msg);
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+impl Drop for ShellIcon {
+
+    fn drop(&mut self) {
+        unsafe {
+            if Shell_NotifyIconW(NIM_DELETE, &mut self.0) != TRUE { panic!("Error removing tray icon") };
+        }
     }
 }
 
-unsafe extern "system" fn window_procedure(hwnd: HWND, msg: UINT, w_param : WPARAM, l_param: LPARAM) -> LRESULT {
+unsafe extern "system" fn tray_window_proc(hwnd: HWND, msg: UINT, w_param : WPARAM, l_param: LPARAM) -> LRESULT {
     match msg {
         CALLBACK_MSG => {
             match LOWORD(l_param as DWORD) as u32 {
                 WM_LBUTTONUP | WM_RBUTTONUP  => {
-                    println!("Clicked");
+                    display_ui();
                 }
                 _ => {}
             }
