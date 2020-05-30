894ab192a42512235789f762ae25f0fe4c108f9a
Jacob Halsey <jacob@jhalsey.com>
2020-05-30 17:48:03 +0100

    generate bindings

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 #![windows_subsystem = "windows"]
 
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+mod ssc;
+
 mod assets;
 mod tray;
 mod wide;
 mod console;
 mod tui;
 mod config;
+
 
 use crate::tray::TrayApplication;
 
