328887c94929198707478966a96a7b1433d585cc
Jacob Halsey <jacob@jhalsey.com>
2020-06-03 17:09:54 +0100

    better panic handling

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Desktop PCs and computers without an ambient light sensor.
 This changes the monitor screen brightness via the DDC/CI monitor APIs, whereas those utilities vary the colour tone of the display.
 
 #### Platform Support
-Currently windows only, however the algorithm is in portable C, the user interface uses `crossterm`, to make fully cross platform just requires replacing some Win32 API calls with equivalents. 
+Currently windows only, however the algorithm is in portable C, the user interface uses `crossterm`, to make fully cross platform would require replacing some Win32 API calls with equivalents. 
 
 #### Features
 - An icon appears in the tray when running
--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,31 @@ mod brightness;
 use crate::tray::TrayApplication;
 use crate::config::Config;
 use crate::brightness::{start_loop, BrightnessLoopMessage};
+use winapi::_core::panic::PanicInfo;
+use winapi::um::winuser::{MessageBoxW, MB_OK, MB_ICONSTOP};
+use crate::wide::WideString;
+use winapi::shared::windef::HWND;
+use winapi::shared::ntdef::NULL;
 
-fn main() -> Result<(), &'static str> {
-    check_for_duplicate()?;
+
+fn main() {
+    std::panic::set_hook(Box::new(handle_panic));
+    if already_running() { panic!("Already running") };
     let config = Config::load().unwrap_or(Config::default());
     let (sender, status) = start_loop(config);
     let app = TrayApplication::create(sender.clone(), status);
     app.run();
     sender.send(BrightnessLoopMessage::Exit).unwrap();
-    Ok(())
 }
 
-fn check_for_duplicate() -> Result<(), &'static str> {
-    //Err("Already running")
-    Ok(())
+fn already_running() -> bool {
+    false
+}
+
+fn handle_panic(info: &PanicInfo) {
+    unsafe {
+        let title = "Fatal Error".to_wide();
+        let text = format!("{}", info).as_str().to_wide();
+        MessageBoxW(NULL as HWND, text.as_ptr(), title.as_ptr(), MB_OK | MB_ICONSTOP);
+    }
 }
